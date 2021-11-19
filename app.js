1e7fa48f784c069203f12be190c8b2a2394163b0
Jacob Mayfield <jacobmayfield899@gmail.com>
2021-11-18 23:17:48 -0500

    Fixed quotes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const quoteElement = document.getElementById('quote')
 const authorElement = document.getElementById('author')
 const quoteButton = document.getElementById('quoteBtn')
 
+const apiURL = "https://icanhazdadjoke.com/"
+
 quoteBtn.addEventListener('click', generateQuote)
 
 generateQuote()
@@ -12,13 +14,12 @@ async function generateQuote() {
             Accept: 'application/json',
         }
     }
-
-    const res = await fetch('https://api.themotivate365.com/stoic-quote')
+    const res = await fetch(apiURL,config)
     const data = await res.json()
+    quoteElement.innerHTML = data.joke
+
+    const dadNames = ["Bill","Dan","Dave","Doug","Ed","Frank","Greg","Jeff","Jim","Larry","Martin","Mike","Pete","Ray","Richard","Steve","Tom","Tim"]
+    authorElement.innerHTML = dadNames[Math.floor(Math.random() * dadNames.length)]
     
-    const { quote, author } = data.results
-
-    quoteElement.innerHTML = quote
-    authorElement.innerHTML = author
-
+    document.body.style.backgroundImage = 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')';
 }
